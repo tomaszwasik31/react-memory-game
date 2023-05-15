8b79e760f8e4dd8580f557512956707de3734502
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-05-15 17:48:56 +0100

    working on logic, memory leak to fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,75 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Cards from "./components/Cards";
+import Card from "./components/Card";
 import cardData from "./cardsData";
 
 function App() {
-  const [cards, setCards] = useState(cardData);
-
-  // This function shuffles the cards and selects 9 of them.
-  // It checks if at least one of the selected cards has inMemory property set to false.
-  // If yes, it returns the shuffled cards.
-  // If not, it shuffles again and repeats the process until a card with inMemory: false is found.
+  const [cards, setCards] = React.useState(cardData);
+  const [streak, setStreak] = React.useState(0);
 
   const getRandomCards = () => {
+    // Shuffle the cards array randomly and select the first 9 cards.
     const shuffled = cards.sort(() => 0.5 - Math.random()).slice(0, 9);
-    return shuffled.some((card) => !card.inMemory)
-      ? shuffled
-      : getRandomCards();
+
+    // Check if at least one of the selected cards has inMemory property set to false.
+    const hasMemoryCard = shuffled.some((card) => !card.inMemory);
+    if (hasMemoryCard) {
+      // If there is at least one card with inMemory: false, return the shuffled cards.
+      return shuffled;
+    } else {
+      // If none of the cards have inMemory: false, shuffle again and try again.
+      return getRandomCards();
+    }
+  };
+  const [shuffledCards, setShuffledCards] = React.useState(getRandomCards());
+
+  const cardClick = (id) => {
+    console.log(cards);
+
+    setCards((prevCards) =>
+      prevCards.map((card) => {
+        if (card.id === id && card.inMemory === false) {
+          console.log("change");
+          return { ...card, inMemory: true };
+        } else if (card.id === id && card.inMemory === true) {
+          console.log("game over");
+          return resetGame();
+        } else {
+          return card;
+        }
+      })
+    );
+    setStreak((prevStreak) => prevStreak + 1);
+    setShuffledCards(getRandomCards());
   };
 
-  const shuffledCards = getRandomCards();
+  function resetGame() {
+    // reset the game by setting all cards' inMemory to false and shuffling the cards
+    setCards((prevCards) =>
+      prevCards.map((card) => ({ ...card, inMemory: false }))
+    );
+    setStreak(0);
+  }
+
+  const cardsElements = shuffledCards.map((card) => (
+    <Card
+      key={card.id}
+      id={card.id}
+      img={card.img}
+      name={card.name}
+      handleClick={() => cardClick(card.id)}
+    />
+  ));
 
   return (
     <div className="app-container">
       <div className="txt-container">
-        {" "}
         <h1>Memory game</h1>
         <h2>
-          Streak: 0 <br /> Record: 5
+          Streak: {streak} <br /> Record: 5
         </h2>
       </div>
-
-      <Cards shuffledCards={shuffledCards} />
+      <div className="cards-container">{cardsElements}</div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card(props) {
+  return (
+    <div
+      className="card"
+      key={props.id}
+      id={props.id}
+      onClick={props.handleClick}
+    >
+      <img className="card-img" src={props.img} />
+      <p className="card-name"> {props.name} </p>
+    </div>
+  );
+}
